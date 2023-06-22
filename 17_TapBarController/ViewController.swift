794d55d9bbc94af4e9ac0ad38203be8cfcd43d97
Dmitry Komissarov <komdimkom@gmail.com>
2023-06-22 12:33:48 +0300

    Initial Commit

---
+++ 17_TapBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  17_TapBarController
+//
+//  Created by mac on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
